25d1d467f8e4c4e241ed22ee777a237525e94324
Pimlada Kongkham <pkongkham@ufl.edu>
2021-06-05 23:42:36 -0400

    added function

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var pizza = {
+    crust: "bread",
+    cheese: "bree",
+    sauce: 5,
+    addToppings: function() {
+        console.log("Please add pepperoni and sausage");
+    }
+}
+
+!function orderPizza() {
+    console.log(pizza)
+  }();
+
